18986393c8d2da7073b49c5307600b2521a5d1a7
khs <hyeongs2323@gmail.com>
2023-08-19 21:44:01 +0900

    feat : scroll first func add

--- src/pages/components/Sections.tsx
+++ src/pages/components/Sections.tsx
@@ -1,9 +1,11 @@
+import { useEffect, useRef, useState } from "react";
 import { VStack } from "@chakra-ui/react";
 import { Profile } from "./profile";
-import { motion } from "framer-motion";
 import { Description } from "./description";
 import { Portfolio } from "./portfolio";
 import { Contact } from "./contact";
+import { scrollToSection } from "../lib/scrollToSection";
+import { useDidMountEffect } from "../hooks/useDidMountEffect";
 
 interface ISection {
   name: string;
@@ -18,6 +20,45 @@ export const sections: ISection[] = [
 ];
 
 export const Sections = () => {
+  const [activeSection, setActiveSection] = useState(0);
+  const scrollTimeoutRef = useRef<NodeJS.Timeout | null>(null);
+
+  useDidMountEffect(() => {
+    const handleWheel = (e: WheelEvent) => {
+      if (scrollTimeoutRef.current) {
+        clearTimeout(scrollTimeoutRef.current);
+      }
+
+      const deltaY = e.deltaY;
+      const scrollDirection = deltaY > 0 ? "up" : "down";
+
+      const delay = 500;
+
+      scrollTimeoutRef.current = setTimeout(() => {
+        console.log("scroll", scrollDirection, activeSection);
+
+        if (scrollDirection === "down" && activeSection < 4) {
+          setActiveSection((i) => i + 1);
+        } else if (scrollDirection === "up" && activeSection > 0) {
+          setActiveSection((i) => i - 1);
+        }
+      }, delay);
+    };
+
+    document.addEventListener("wheel", handleWheel);
+
+    return () => {
+      if (scrollTimeoutRef.current) {
+        clearTimeout(scrollTimeoutRef.current);
+      }
+      document.removeEventListener("wheel", handleWheel);
+    };
+  }, []);
+
+  useEffect(() => {
+    scrollToSection(sections[activeSection].name);
+  }, [activeSection]);
+
   return (
     <VStack w="100%" justifyContent="center">
       {sections.map((section: ISection) => (
