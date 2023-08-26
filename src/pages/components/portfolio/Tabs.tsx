789c2ebd2523ac59ba413250c9bbf2c8f4bb8860
khs <hyeongs2323@gmail.com>
2023-08-26 14:57:01 +0900

    feat : tabs update

--- src/pages/components/portfolio/Tabs.tsx
+++ src/pages/components/portfolio/Tabs.tsx
@@ -18,15 +18,28 @@ const ppts = [
 
 export const PptTabs = () => {
   return (
-    <Tabs colorScheme="facebook">
-      <TabList>
+    <Tabs colorScheme="facebook" display="flex">
+      <TabList
+        h="100%"
+        my="5"
+        gap="1"
+        display="flex"
+        flexDir="column"
+        borderRadius="lg"
+        boxShadow="md"
+      >
         {ppts.map((p: IPPT, index: number) => (
-          <Tab key={index}>
+          <Tab
+            key={index}
+            flex="1"
+            minH="50px"
+            borderRadius="lg"
+            _selected={{ backgroundColor: "lightgray" }}
+          >
             <Title title={p.title} isNew={p.isNew} />
           </Tab>
         ))}
       </TabList>
-
       <TabPanels display="flex" justifyContent="center">
         {ppts.map((ppt: IPPT, index: number) => (
           <TabPanel key={index}>
